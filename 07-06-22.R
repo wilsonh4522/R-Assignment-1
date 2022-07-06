158d2524c56c484692c4d304f95a257cb49a6a12
T&T Development <99367973+terrathedeveloper@users.noreply.github.com>
2022-07-06 14:17:42 -0500

    first R Assignment

---
+++ 07-06-22.R
@@ -0,0 +1,28 @@
+# R Assignment 1
+
+
+# Create a variable 'x' that holds a value of 9
+
+# Create a variable 'y' that holds a value of x
+
+# Create a variable 'z' that holds the sum of x and y
+
+# Clear the variable x from the workspace
+
+# Clear all variables from the workspace
+
+# Make a statement that directs to http://www.google.com
+
+# Show a list of all packages installed
+
+# Show a list of packages currently installed and usable
+
+
+
+# Magic Number Exercise
+# Do the Following:
+    # Choose any number and add 2 to it.
+    # Multiply the result by 3.
+    # Subtract 6 from the answer.
+    # Divide what you get by 3.
+# you should end up with the same number that you started with
